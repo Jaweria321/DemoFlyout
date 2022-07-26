643b86aae655d4defb176ed0226a1e4b3ec0718c
Jaweria321 <jaweria.ehsan123@gmail.com>
2022-07-27 01:48:15 +0500

    Add project files.

---
+++ DemoSqlite/DemoSqlite/FlyoutItemPage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoSqlite
+{
+    class FlyoutItemPage
+    {
+
+        public string Title { get; set; }
+        public string IconSource { get; set; }
+        //targetPage variable is where we store our page. Its datatype is Type.
+        //it represents type declarations such as class types, interface types, array types, enumeration types and so on
+        public Type TargetPage { get; set; }
+        
+    }
+}
